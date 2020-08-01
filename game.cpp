90366488405be470acec7c82979c84c8322b9d14
ybeen <byb09172@naver.com>
2020-08-01 18:30:19 +0900

    menuDraw

--- game.cpp
+++ game.cpp
@@ -22,8 +22,50 @@ int keyControl() {
 
 void titleDraw() {
 	cout << "\n\n\n\n";
-	cout << "        ¦¢¦¢¦¢¦¢  ¥Ë    ¦¢¦¢¦¢¦¢  ¥Ë";
-	cout << "        ¦§¦«¦©¦¢ £¯ ¡¬  ¦§¦«¦©¦§ £¯ ¡¬" ;
-	cout << "        ¦¢¦¢¦¢¦¢ ¦¨¦¡¦¨ ¦¢¦¢¦¢¦¢ ¦¨¦¡¦¨";
-	cout << "        ¦¦¦ª¦¥¦¢  £¼£¾  ¦¦¦ª¦¥¦¢  £¼£¾";
+	cout << "        ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½  ï¿½ï¿½    ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½  ï¿½ï¿½";
+	cout << "        ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ ï¿½ï¿½  ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ ï¿½ï¿½" ;
+	cout << "        ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½";
+	cout << "        ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½  ï¿½ï¿½ï¿½ï¿½  ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½  ï¿½ï¿½ï¿½ï¿½";
+}
+
+int menuDraw(){
+    int x = 24;
+    int y = 12;
+    gotoxy(x-2,y); //-2 for "> "
+    cout << "> GAME START";
+    gotoxy(x,y+1);
+    cout << "HELP";
+    gotoxy(x,y+2);
+    cout << "HIGH SCORE";
+    gotoxy(x,y+3);
+    cout << "EXIT";
+    while(1){
+		int n = keyControl();
+		switch(n){
+			case UP: {
+				if(y>12){//12<=y<=15
+					gotoxy(x-2,y);
+					cout << " "; //erase >
+					gotoxy(x-2,--y);
+					cout << ">";
+				}
+				break;
+			}
+			
+			case DOWN:{
+					if(y<15){
+							gotoxy(x-2,y);
+							cout << " ";
+							gotoxy(x-2,++y);
+							cout << ">";
+					}
+					break;
+			}
+
+			case SUBMIT:{
+				return y-12; // gamestart:0, help:1, highscore:2, exit:3
+			}
+		}
+	}
+
 }
