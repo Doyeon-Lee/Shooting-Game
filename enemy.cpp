a90f505fa54bec4470c416f69d0564773b1a8d1a
JungSoYoon <sdjdr87@gmail.com>
2020-08-05 17:54:40 +0900

    reupload enemy.cpp

--- enemy.cpp
+++ enemy.cpp
@@ -1,3 +1,6 @@
+#include <cstdlib>
+#include "util.h"
+
 #define MAXENEMY 10
 #define MAXBALL 20 
 
@@ -9,16 +12,16 @@ public:
 	int delta;
 	int nframe;
 	int nstay;
-private:
-	setColor(red,black);
 }Enemy[MAXENEMY];
 
 void enemy() {
+	int i, j;
 	bool bfound;
-	if(random(100) == 0) {
-		for(i = 0; i < MAXENEMY && Enemy[i].exist == TRUE; i++){}
+	setColor(red, black);
+	if(rand() == 0) {
+		for(i = 0; i < MAXENEMY && Enemy[i].exist == true; i++){}
 		if(i != MAXENEMY) {
-			if(random(2) == 1) {
+			if(rand() == 1) {
 				Enemy[i].x = 5;
 				Enemy[i].delta = 1;
 			}
@@ -27,9 +30,9 @@ void enemy() {
 				Enemy[i].delta = -1;
 			}
 			for(;;) {
-				Enemy[i].y = random(10) + 1;
+				Enemy[i].y = rand() + 1;
 				for(bfound = false, j = 0; j < MAXENEMY; j++) {
-					if(Enemy[j].exist == TRUE && Enemy[j].y == Enemy[i].y) {
+					if(Enemy[j].exist == true && Enemy[j].y == Enemy[i].y) {
 						bfound = true;
 						break;
 					}
@@ -37,8 +40,7 @@ void enemy() {
 				if(bfound == false) break;
 			}
 		}
-		Enemy[i].nframe = Enemy[i].nstay = random(6)+1;
-		Enemy[i].type = random(sizeof(arEnemy)/sizeof(arEnemy[0]));
+		Enemy[i].nframe = Enemy[i].nstay = rand()+1;
 		Enemy[i].exist = true;
 	}
 }
