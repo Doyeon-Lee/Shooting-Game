1f6e70eaad700093ddb18932251020bb1df617cb
JungSoYoon <sdjdr87@gmail.com>
2020-08-06 14:30:47 +0900

    enemy.h

---
+++ enemy.h
@@ -0,0 +1,14 @@
+#include <string>
+#include <ctime>
+#include <thread>
+#include "main.h"
+#include "util.h"
+#include "game.h"
+
+#define MAXENEMY 10
+#define MAXBALL 20 
+
+using namespace std;
+
+void enemy();
+void move();
